24c1529fdf2165612dd2119f0df1b6b8ab1f91bd
ganeshabm <gbm8970@gmail.com>
2023-11-12 20:59:10 +0530

    updated commit

---
+++ assets/details.js
@@ -0,0 +1,121 @@
+{
+  "78294620xx": {
+    "name": "Ganesha B M",
+    "Programme": "MCA1 - MASTER OF COMPUTER APPLICATIONS",
+    "College_Code": "1001 - JSS STU, SJCE, Mysuru",
+    "Exam_Center": "1002 - JSS STU, SJCE, Mysuru",
+    "image": "01JST22PMC018.jpeg",
+    "year": "OCTOBER 2023",
+    "usn": "01JST22PMC018",
+    "studentid": "22001294",
+    "subject": [
+      ["II", "22MCA210", "JAVA AND J2EE", "02/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA220", "ADVANCED ALGORITHMS", "04/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA230", "SOFTWARE ENGINEERING", "06/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA262", "NOSQL", "08/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22PNI260OE", "ADVANCED WIRELESS TECHNOLOGIES -OE", "16/11/2023 - 09:30AM to 12:30PM"]
+    ]
+  },
+  "94815548xx": {
+    "name": "Adithya Jain M.S",
+    "Programme": "MCA1 - MASTER OF COMPUTER APPLICATIONS",
+    "College_Code": "1001 - JSS STU, SJCE, Mysuru",
+    "Exam_Center": "1002 - JSS STU, SJCE, Mysuru",
+    "image": "01JST22PMC001.jpeg",
+    "year": "OCTOBER 2023",
+    "usn": "01JST22PMC001",
+    "studentid": "22001277",
+    "subject": [
+      ["II", "22MCA210", "JAVA AND J2EE", "02/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA220", "ADVANCED ALGORITHMS", "04/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA230", "SOFTWARE ENGINEERING", "06/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA262", "NOSQL", "08/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22PNI260OE", "ADVANCED WIRELESS TECHNOLOGIES -OE", "16/11/2023 - 09:30AM to 12:30PM"]
+    ]
+  },
+  "96115631xx": {
+    "name": "Deepak S G",
+    "Programme": "MCA1 - MASTER OF COMPUTER APPLICATIONS",
+    "College_Code": "1001 - JSS STU, SJCE, Mysuru",
+    "Exam_Center": "1002 - JSS STU, SJCE, Mysuru",
+    "image": "01JST22PMC015.jpeg",
+    "year": "OCTOBER 2023",
+    "usn": "01JST22PMC015",
+    "studentid": "22001294",
+    "subject": [
+      ["II", "22MCA210", "JAVA AND J2EE", "02/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA220", "ADVANCED ALGORITHMS", "04/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA230", "SOFTWARE ENGINEERING", "06/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA262", "NOSQL", "08/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22PNI260OE", "ADVANCED WIRELESS TECHNOLOGIES -OE", "16/11/2023 - 09:30AM to 12:30PM"]
+    ]
+  },
+  "81059529xx": {
+    "name": "Pradeep B Y",
+    "Programme": "MSCS1 - M.Sc. in COMPUTER SCIENCE (CYBER SECURITY)",
+    "College_Code": "1001 - JSS STU, SJCE, Mysuru",
+    "Exam_Center": "1002 - JSS STU, SJCE, Mysuru",
+    "image": "01JST22PCS011.jpeg",
+    "year": "OCTOBER 2023",
+    "usn": "01JST22PCS011",
+    "studentid": "22001408",
+    "subject": [
+      ["II", "22MSCS210", "INTRODUCTION TO PYTHON PROGRAMMING", "31/10/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MSCS220", "MACHINE LEARNING IN CYBER SECURITY", "02/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MSCS230", "INTERNET OF THINGS AND SECURITY", "04/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MSCS243", "BIG DATA ANALYTICS AND VISUALIZATION", "06/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA270E", "INTELLIGENT APPLICATIONS", "16/11/2023 - 09:30AM to 12:30PM"]
+    ]
+  },
+  "97416365xx": {
+    "name": "Pooja M S",
+    "Programme": "MCA1 - MASTER OF COMPUTER APPLICATIONS",
+    "College_Code": "1001 - JSS STU, SJCE, Mysuru",
+    "Exam_Center": "1002 - JSS STU, SJCE, Mysuru",
+    "image": "01JST22PMC046.jpeg",
+    "year": "OCTOBER 2023",
+    "usn": "01JST22PMC046",
+    "studentid": "22001294",
+    "subject": [
+      ["II", "22MCA210", "JAVA AND J2EE", "02/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA220", "ADVANCED ALGORITHMS", "04/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA230", "SOFTWARE ENGINEERING", "06/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA262", "NOSQL", "08/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22PNI260OE", "ADVANCED WIRELESS TECHNOLOGIES -OE", "16/11/2023 - 09:30AM to 12:30PM"]
+    ]
+  },
+  "63601613xx": {
+    "name": "Monisha M B",
+    "Programme": "MCA1 - MASTER OF COMPUTER APPLICATIONS",
+    "College_Code": "1001 - JSS STU, SJCE, Mysuru",
+    "Exam_Center": "1002 - JSS STU, SJCE, Mysuru",
+    "image": "01JST22PMC039.jpeg",
+    "year": "OCTOBER 2023",
+    "usn": "01JST22PMC039",
+    "studentid": "22001294",
+    "subject": [
+      ["II", "22MCA210", "JAVA AND J2EE", "02/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA220", "ADVANCED ALGORITHMS", "04/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA230", "SOFTWARE ENGINEERING", "06/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA262", "NOSQL", "08/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22PNI260OE", "ADVANCED WIRELESS TECHNOLOGIES -OE", "16/11/2023 - 09:30AM to 12:30PM"]
+    ]
+  },
+  "81974253xx": {
+    "name": "Sinchana Ganesh",
+    "Programme": "MCA1 - MASTER OF COMPUTER APPLICATIONS",
+    "College_Code": "1001 - JSS STU, SJCE, Mysuru",
+    "Exam_Center": "1002 - JSS STU, SJCE, Mysuru",
+    "image": "01JST22PMC058.jpeg",
+    "year": "OCTOBER 2023",
+    "usn": "01JST22PMC058",
+    "studentid": "22001294",
+    "subject": [
+      ["II", "22MCA210", "JAVA AND J2EE", "02/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA220", "ADVANCED ALGORITHMS", "04/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA230", "SOFTWARE ENGINEERING", "06/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22MCA262", "NOSQL", "08/11/2023 - 09:30AM to 12:30PM"],
+      ["II", "22PNI260OE", "ADVANCED WIRELESS TECHNOLOGIES -OE", "16/11/2023 - 09:30AM to 12:30PM"]
+    ]
+  }
+}
--- index.js
+++ index.js
@@ -1,17 +1,89 @@
-const fs = require('fs');
-const http = require('http');
-const url = require('url');
-const express = require('express');
-const app = express();
-app.use(express.static(__dirname));
-let data = fs.readFileSync('./hallTicket.html', 'utf-8');
+const fs = require("fs")
+const http = require("http")
+const url = require("url")
+const express = require("express")
+const app = express()
+app.use(express.static(__dirname))
+const PORT = process.env.PORT || 8080
+let data = fs.readFileSync("./hallTicket.html", "utf-8")
+let index = fs.readFileSync("./index.html", "utf-8")
+let credentials = fs.readFileSync("./assets/logindetails.js", "utf-8")
+let details = fs.readFileSync("./assets/details.js", "utf-8")
+let currentDate = new Date()
+const options = {
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+  hour: "numeric",
+  minute: "numeric",
+  hour12: true
+}
+const formattedDate = new Intl.DateTimeFormat("en-US", options).format(
+  currentDate
+)
 const sendHallticket = (req, res) => {
-  res.set('Content-Type', 'text/html');
-  res.status(200);
-  res.send(data); // Serve the HTML file
-};
-app.get('/', sendHallticket);
-app.get('/download', sendHallticket);
-app.listen(8000, () => {
-  console.log('initiating ' + 8000);
-});
+  res.set("Content-Type", "text/html")
+  res.status(200)
+  res.send(data) // Serve the HTML file
+}
+let output = ""
+const renderHallTicket = (dataStr, resdetails, det) => {
+  let i = 1
+  const valDet = resdetails[`${det}`]
+  let val = dataStr.replace(/%yearMonth%/g, valDet.year)
+  val = val.replace(/%program%/g, valDet.Programme)
+  val = val.replace(/%collegeCode%/g, valDet.College_Code)
+  val = val.replace(/%studentName%/g, valDet.name)
+  val = val.replace(/%photoPath%/g, valDet.image)
+  val = val.replace(/%examCenter%/g, valDet.Exam_Center)
+  val = val.replace(/%dateAndTime%/g, formattedDate)
+  let usnNo = valDet.usn.split("").map((val) => {
+    return `<th>${val}</th>`
+  })
+  let stdid = valDet.studentid.split("").map((val) => {
+    return `<th>${val}</th>`
+  })
+  val = val.replace("%usn%", usnNo.join(""))
+  val = val.replace("%studentId%", stdid.join(""))
+  let tableVal = valDet.subject.map((val) => {
+    tableval = `<tr>
+      <td>${i}</td>
+      <td>${val[0]}</td>
+      <td>${val[1]}</td>
+      <td>${val[2]}</td>
+      <td>${val[3]}</td>
+      <td></td>
+      <td></td>
+    </tr>`
+    i += 1
+    return tableval
+  })
+  val = val.replace("%table%", tableVal.join(" "))
+  return val
+}
+const login = (req, res) => {
+  ;({
+    query: { username, password }
+  } = url.parse(req.url, true))
+  output = JSON.parse(credentials).filter((val) => {
+    if (val[0] === username && val[1] === password) return val
+  })
+  if (output != "") {
+    res.set("Content-Type", "text/html")
+    res.status(200)
+    const renderData = renderHallTicket(data, JSON.parse(details), username)
+    res.send(renderData)
+  } else {
+    let errorData = index.replace(
+      /%error%/g,
+      "Login Failed!!! Credentials Are Wrong"
+    )
+    res.send(errorData)
+  }
+}
+// Create HTTP server and listen on port 3000
+app.get("/download", sendHallticket)
+app.get("/login", login)
+app.listen(PORT, () => {
+  console.log(`initiating ${PORT}`)
+})
